5edceeff2373e81dc2766501c412839c171d41e6
Kyle-858 <krazykyle19@gmail.com>
2025-09-23 19:33:58 -0400

    Nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -5,7 +5,21 @@ import bandname_img from '../assets/bandname_img.png'
 const Nav = () => {
   return (
     <nav>
-      <img className="nav-logo" src={bandname_img} alt="" />
+        <div className="nav-links">
+            <div className="nav-left">
+                <div className="nav-link">Home</div>
+                <div className="nav-link">Band</div>
+                <div className="nav-link">Tour</div>
+                <div className="nav-link">News</div>
+            </div>
+            <img className="nav-logo" src={bandname_img} alt="" />
+            <div className="nav-right">
+                <div className="nav-link">Music</div>
+                <div className="nav-link">Media</div>
+                <div className="nav-link">Shop</div>
+                <div className="nav-link">Foundation</div>
+            </div>
+        </div>
     </nav>
   )
 }
